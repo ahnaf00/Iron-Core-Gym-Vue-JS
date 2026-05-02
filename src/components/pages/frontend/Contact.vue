53e6caebae7c8fd09194f7359ded2e2680ad7e9c
Ahnaf Anan <aanana0171@gmail.com>
2026-05-03 00:53:53 +0600

    Contact name updated

---
+++ src/components/pages/frontend/Contact.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Footer from '../../common/Footer.vue';
+import Header from '../../common/Header.vue';
+import ContactHeader from '../../frontend/contact/ContactHeader.vue';
+import ContactSections from '../../frontend/contact/ContactSections.vue';
+
+
+</script>
+
+<template>
+        <main class="flex-grow pt-20">
+            <Header/>
+            <ContactHeader/>
+            <ContactSections/>
+            <Footer/>
+        </main>
+</template>
+
+<style scoped>
+
+</style>
